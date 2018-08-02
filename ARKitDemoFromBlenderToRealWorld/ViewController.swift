4e3095657e6d86358f12bcc42d165b92aaa7c8c1
florianmaxim <hello@florianmaxim.com>
2018-08-03 01:01:16 +0200

    Hello Augmented Reality

--- ARKitDemoFromBlenderToRealWorld/ViewController.swift
+++ ARKitDemoFromBlenderToRealWorld/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var sceneView: ARSCNView!
     
+    var i = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -50,20 +52,41 @@ class ViewController: UIViewController {
         let tapLocation = recognizer.location(in: sceneView)
         
         // Using automatically detected planes
-        //let hitTestResults = sceneView.hitTest(tapLocation, types: .existingPlaneUsingExtent)
+        let hitTestResults = sceneView.hitTest(tapLocation, types: .existingPlaneUsingExtent)
         // Using automatically detected feature points
-        let hitTestResults = sceneView.hitTest(tapLocation, types: .featurePoint)
+        //let hitTestResults = sceneView.hitTest(tapLocation, types: .featurePoint)
         
         
         guard let hitTestResult = hitTestResults.first else { return }
         let translation = hitTestResult.worldTransform.translation
         let x = translation.x
-        let y = translation.y
+        let y = translation.y + 0.1
         let z = translation.z
         
+        func random(_ range:Range<Int>) -> Int
+        {
+            return range.lowerBound + Int(arc4random_uniform(UInt32(range.upperBound - range.lowerBound)))
+        }
+        
+
+        var modelString = ""
+        
+        switch random(0..<3) {
+        case 1:
+            modelString = "stairs"
+        case 2:
+            modelString = "tube"
+        case 3:
+            modelString = "spiral"
+        default:
+            modelString = "spiral"
+        }
+        
         guard let shipScene = SCNScene(named: "models.scn"),
-            let shipNode = shipScene.rootNode.childNode(withName: "spiral", recursively: false)
+            let shipNode = shipScene.rootNode.childNode(withName: modelString, recursively: false)
             else { return }
+        
+        i = !i
         
         var actionRotate = SCNAction.rotateBy(x: 1.25, y: 0, z: 1.5, duration: 8)
             actionRotate = SCNAction.repeatForever(actionRotate)
@@ -75,14 +98,23 @@ class ViewController: UIViewController {
         
             shipNode.runAction(action)
         
-        /*
-        var arrayOfColors = [UIColor.red, UIColor.yellow]
-        let randomColor = arc4random() % UInt32(arrayOfColors.count)
-        let color = arrayOfColors[Int(randomColor)]
+        func generateRandomColor() -> UIColor {
+            
+            // use 256 to get full range from 0.0 to 1.0
+            let hue : CGFloat = CGFloat(arc4random() % 256) / 256
+            // from 0.5 to 1.0 to stay away from white
+            let saturation : CGFloat = CGFloat(arc4random() % 128) / 256 + 0
+            // from 0.5 to 1.0 to stay away from black
+            let brightness : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.5
+            
+            return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: 1)
+        }
         
-        shipNode.geometry?.materials.first?.diffuse.contents = color
-        shipNode.geometry?.materials.first?.selfIllumination.contents = UIColor.blue
-        */
+        shipNode.geometry?.materials.first?.lightingModel = .blinn
+        shipNode.geometry?.materials.first?.shininess = 0.5
+        //shipNode.geometry?.materials.first?.reflective.contents = generateRandomColor()
+        shipNode.geometry?.materials.first?.diffuse.contents = generateRandomColor()
+        shipNode.geometry?.materials.first?.selfIllumination.contents = UIColor.white
         
         shipNode.position = SCNVector3(x,y,z)
         sceneView.scene.rootNode.addChildNode(shipNode)
